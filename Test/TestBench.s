99f90766a7010b7b3843fffdc12a07584914fff6
Marcelo <marcelojunqueiraf@gmail.com>
2021-10-28 19:35:55 -0300

    processador pronto

--- Test/TestBench.s
+++ Test/TestBench.s
@@ -1,78 +1,155 @@
 .data 
 Lugar: .word 0
+.eqv pc 0xF08AE
+.eqv sum 0xf24
 .text
 	addi a1, zero 2
 	addi a2, zero -2
 	add a0, a1, a2
+	nop #
+	nop #
 	bne a0,zero, Erro
+	nop #
 	andi a0, a2, 0
+		nop #
+	nop #
 	beq a0, zero, OK1
+	nop #
 	jal ra, Erro
+	nop #
 OK1: 	ori a5, zero,255
 	ori a6,a5,0
+		nop #
+	nop #
 	bge a6,a5, OK2
+	nop #
 	jal ra, Erro
-OK2: 	bgeu a6,a5, OK3
+	nop #
+OK2: 	nop #
+	nop #
+	bgeu a6,a5, OK3
+	nop #
 	jal ra, Erro
+	nop #
 OK3:	addi a1, zero, 2
 	addi a2, zero, 1
 	sub a3, a1, a2
+	nop #
+	nop #
 	blt  a3, a1, OK4
+	nop #
 	jal ra, Erro
+	nop #
 OK4:	addi a1, zero, 0x2
 	and a3, a1, zero
+	nop #
+	nop #
 	beq a3, zero, OK5
+	nop #
 	jal ra, Erro
+	nop #
 OK5:	or a3, a1, zero
+	nop #
+	nop #
 	bltu zero, a3, OK6
+	nop #
 	jal ra, Erro
+	nop #
 OK6:	addi a2,zero, 2
 	addi a3, zero, 3
 	slt  a1, a2,a3
 	sltu a0, a2, a3
+	nop #
+	nop #
 	beq a0,a1, OK7
+	nop #
 	jal ra, Erro
+	nop #
 OK7:	addi a0, zero, 0
 	xori a0, zero 0x2
+	nop #
+	nop #
 	bne a0, zero, OK8
+	nop #
 	jal ra, Erro
+	nop #
 OK8:	addi a3, zero, 1
 	slti a2, a0, 3
+	nop #
+	nop #
 	beq a2, a3, OK9
+	nop #
 	jal ra, Erro
+	nop #
 OK9:	sltiu a1, a0, -2
+	nop #
+	nop #
 	beq a1, a3, OK10
+	nop #
 	jal ra, Erro
+	nop #
 OK10:	addi a1, zero, 1
 	addi a0, zero, 2
 	sll a1, a1, a1
+	nop #
+	nop #
 	beq a1,a0, OK11
+	nop #
 	jal ra, Erro
+	nop #
 OK11:   slli a1, a1, 0
+	nop #
+	nop #
 	beq a1, a0, OK12
+	nop #
 	jal ra, Erro
+	nop #
 OK12:	lui a1, 0
+	nop #
+	nop #
 	beq a1, zero, OK13
+	nop #
 	jal ra, Erro
-OK13:	auipc a0, 0xF08A1
-	li a1, 0xF0CA10D4
+	nop #
+OK13:	auipc a0, pc
+	li t0, sum
+	add a0,a0,t0
+	li a1, 0xF0CAF0D4
 	lui a2, 0x10010
+	nop #
 	sw a1, 0(a2)
 	lw a3, 0(a2)
+	nop #
+	nop #
 	beq a3, a0, OK14
+	nop #
 	jal ra, Erro
+	nop #
 OK14:	addi a0, zero, 0
 	addi a1, zero, 0x2
 	xor a0, zero, a1
+	nop #
+	nop #
 	bne a0, zero, OK15
+	nop #
 	jal ra, Erro
+	nop #
 OK15:	jal a0, JALR
-JALR:	jalr a1, a0, 4
-	addi a1, a1, -4
+	nop #
+JALR:	nop #
+	jalr a1, a0, 12
+	nop #
+	addi a1, a1, -12
+	nop #
+	nop #
 	beq a0, a1, OK16
+	nop #
 	jal ra, Erro
+	nop #
 OK16:	li a0, 0xCCCCCCCC
 FimS:	jal ra, FimS
+	nop #
 
 Erro: li a0, 0xEEEEEEEE
 Fim: jal ra, Fim
+	nop #
